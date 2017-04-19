f7e8ce856d5e1a5fad1e1a6a69a60105d28472fa
Eric <eric.tillberg@gmail.com>
2017-04-18 20:44:05 -0400

    Add Rijksmuseum API call to App component

--- components/App.jsx
+++ components/App.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import Work from './Work';
+import config from 'config';
 
 export default class App extends Component {
+  componentWillMount() {
+    const path = config.rijksmuseumUrl;
+    window.fetch(path)
+      .then((res) => {
+        return res.json()
+      })
+      .then((body) => {
+        const object = body.artObject;
+
+        this.setState({
+          work_url: object.webImage.url,
+          title: object.title,
+          artist: object.principalMaker
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  setInitialState(response) {
+    const object = response.artObject;
+
+    this.setState({
+      work_url: object.webImage.url,
+      title: object.title,
+      artist: object.principalMaker
+    });
+  }
+
   render() {
     return (
-      <div></div>
+      <Work />
     )
   }
 }
