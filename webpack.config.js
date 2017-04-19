8dfd56d585f89e9c9384c1b3a8f492210b1ab14e
Eric <eric.tillberg@gmail.com>
2017-04-18 20:44:27 -0400

    Insert Rijksmuseum API key into webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ const config = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(NODE_ENV),
-        RIJKSMUSEUM_KEY: process.env.RIJKSMUSEUM_KEY
+        RIJKSMUSEUM_KEY: JSON.stringify('9QfgAMuT')
       }
     })
   ],
