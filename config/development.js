ae0a586af8b5d7d571a778e5dd49e5912fd24468
Eric <eric.tillberg@gmail.com>
2017-04-22 11:36:52 -0400

    Loading circle displays while fetch is ongoing

--- config/development.js
+++ config/development.js
@@ -1,3 +1,3 @@
 module.exports = {
-  rijksmuseumUrl: `https://www.rijksmuseum.nl/api/en/collection?q=saint&key=${process.env.RIJKSMUSEUM_KEY}&format=json`
+  rijksmuseumUrl: `https://www.rijksmuseum.nl/api/en/collection?q=saint&ps=100&key=${process.env.RIJKSMUSEUM_KEY}&format=json`
 }
